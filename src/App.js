8e4f5219b241f9d700502e815b5a7f18877ec653
Ankur974 <ankurpandey974@gmail.com>
2024-06-04 13:13:28 +0530

    fix:ui and add api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React from "react";
+import Data from "./Data";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>List of top 5 Netflix series</h1>
+      <div className="cards">
+        {Data.map((series, index) => (
+          <div className="card" key={index}>
+            <img src={series.img} alt="Card image" className="card__img" />
+            <div className="card_info">
+              <span className="card_category">A Netflix original</span>
+              <h3 className="card_title">{series.name}</h3>
+              <a
+                href="https://example.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button>Watch Now</button>
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
